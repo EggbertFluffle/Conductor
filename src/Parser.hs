9981afc3add3e40a9854664747ff5026c3f2d228
Parker Lavering <plavering@live.com>
2026-04-05 14:48:11 -0400

    added imports

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,3 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Parser where
+
+
+
+import           Data.Text  
+import           Data.Void        
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
+
