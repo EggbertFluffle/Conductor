606ed39927eb897f3bb51e22ebf00e6bf6587385
Parker Lavering <plavering@live.com>
2026-04-06 11:59:33 -0400

    more parser

--- src/Parser.hs
+++ src/Parser.hs
@@ -10,3 +10,15 @@ import           Text.Megaparsec
 import           Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
 
+
+type Parser = Parser Void Text
+
+-- Define the fuhin comments
+
+sc :: L.space space1 (L.skipLineComment "--") (L.skipBlockComment "{-" "-}")
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme sc 
+
+symbol :: Text -> Parser Text
+symbol = L.symbol sc
