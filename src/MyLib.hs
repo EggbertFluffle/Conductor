c402420ff848ad2ada460242e66ff3bbfbe63701
Harrison DiAmbrosio <hdiambrosio@gmail.com>
2026-04-17 23:57:21 -0400

    starting parser

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,4 +1,17 @@
-module MyLib (someFunc) where
+module Main (main, parseTest) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Parser (parseRule)
+import Data.Text (Text, pack)
+import Text.Megaparsec (parse, errorBundlePretty)
+
+parseTest :: Text -> IO ()
+parseTest input = case parse parseRule "" input of
+    Left err -> putStrLn $ "Error: " ++ errorBundlePretty err
+    Right result -> print result
+
+main :: IO ()
+main = do
+    putStrLn "Testing parser..."
+    parseTest $ pack "start = full | none\n"
+    parseTest $ pack "end = none - full\n"
+	-- parseTest $ pack "start = (full - full) | none\n"
