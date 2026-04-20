bfcd833b5c359bdad822b46683737f37a1e1f5a1
Parker Lavering <plavering@live.com>
2026-04-20 15:47:24 -0400

    Conductor additions

--- src/Conductor.hs
+++ src/Conductor.hs
@@ -1,17 +1,25 @@
 module Conductor (
     parseTest,
     runTests,
-    PointerCoordinate (..),
     ScreenDimension (..),
-    WindowSpec (..),
-    WindowMapping (..),
+    WindowTransform (..),
     WindowLayoutFunc,
+    Config (..),
+    mkConfig,
+    parseConfig,
+    eval,
+    -- re-exports for tests
+    Rule (..),
+    Variable (..),
 ) where
 
+import Control.Monad.Reader (Reader, ask)
 import Data.Aeson (encode)
 import qualified Data.ByteString.Lazy.Char8 as BL
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
 import Data.Text (Text, pack)
-import Parser (parseConductor)
+import Parser (Rule (..), Variable (..), parseConductor)
 import Text.Megaparsec (errorBundlePretty, parse)
 import Prelude hiding (readFile)
 
@@ -49,3 +57,44 @@ data WindowTransform = WindowTransform
 -- XMonad layout call type shi call laypit func
 type WindowLayoutFunc =
     (ScreenDimension, [WindowId]) -> [(WindowId, WindowTransform)]
+
+-- config for conductor
+
+data Config = Config
+    { startingRule :: Rule
+    , rules :: Map Variable Rule
+    }
+    deriving (Show)
+
+-- parse text directly into a config
+parseConfig :: Text -> Either String Config
+parseConfig input = case parse parseConductor "" input of
+    Left err -> Left (errorBundlePretty err)
+    Right rs -> case mkConfig rs of
+        Nothing -> Left "empty program"
+        Just cfg -> Right cfg
+
+-- build config from parsed rules
+
+mkConfig :: [Rule] -> Maybe Config
+mkConfig [] = Nothing
+mkConfig rs@(first : _) =
+    Just $
+        Config
+            { startingRule = first
+            , rules = Map.fromList [(name r, r) | r <- rs]
+            }
+
+-- evaluator 
+
+-- runReader eval cfg :: WindowLayoutFunc
+
+
+eval :: Reader Config WindowLayoutFunc
+eval = do
+    cfg <- ask
+    return $ \(dim, windows) ->
+        evalRule (startingRule cfg) (rules cfg) dim windows
+
+evalRule :: Rule -> Map Variable Rule -> ScreenDimension -> [WindowId] -> [(WindowId, WindowTransform)]
+evalRule = undefined
